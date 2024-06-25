3d4d3d81db028458a2545bbc687cf5fc42107eb7
ChouTed <theodorechoundris@gmail.com>
2024-06-25 18:12:11 +0300

    added

--- src/components/orderPage.vue
+++ src/components/orderPage.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="tableOrderDialog" >
-        <el-input-number class="orderPageTable" v-model="num" @change="handleChange" :min="0" :max="10"></el-input-number>
+        <el-table
+            :data="tableData" style="width: 100%"> 
+        <el-table-column prop="product_name" label="Product Name" width="180">
+        </el-table-column>
+        <el-table-column prop="product_quantity" label="Quantity" width="180">
+            <el-input-number class="orderPageTable" v-model="pproduct_quantity" @change="handleChange" :min="0" :max="10"></el-input-number>
+        </el-table-column>
+    </el-table>
+        
     </div>
 </template>
 
@@ -14,22 +22,29 @@ export default {
         return {
             num: 0,
             productName: 'doulevei to product name',
-    //     customersNumber: 0,
-    //     orderStatusButton: 'Order',
+        //      customersNumber: 0,
+        //      orderStatusButton: 'Order',
+            tableData: [{
+                product_name: 'pad taio',
+                product_quantity: 0,
+            }, 
+            ]
       };
     },
     methods: {
         orderStatus(){
             
         },
-        handleChange(value) {
-        console.log(value)
-        }
+        handleChange() {
+        console.log("patisa order")
+        console.log()
+        },
+
     },
     mounted () {
         console.log("works")
         axios
-            .get('http://localhost:5000/getMenu')
+            .get('http://localhost:5000/api/getMenu')
             .then(response => (this.productName = response))
         console.log(this.productName);
     }
